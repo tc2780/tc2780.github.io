90c4e60d5cff16e62f4990e5bcf9f1c4e9437287
tc2780 <tracyc2780@gmail.com>
2023-01-01 02:19:43 -0800

    started to plan out site, added index, script and style

---
+++ script.js
@@ -0,0 +1,11 @@
+const navBar = document.querySelector('.navbar');
+
+// window.onscroll = function() {
+//     if(window.scrollY >= 100) {
+//         // navBar.classList.add('navbar-dark');
+//         navBar.classList.add('navBarDark');
+//     } else {
+//         // navBar.classList.remove('navbar-dark');
+//         navBar.classList.remove('navBarDark');
+//     }
+// }
